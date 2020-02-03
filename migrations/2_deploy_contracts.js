f0d90b70b877c61b33be9806bd42b0e2be5bd1d6
opz <will.shahda@gmail.com>
2020-02-03 02:25:49 -0500

    Implement migration for SynthRebalancer contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const SynthRebalancer = artifacts.require("SynthRebalancer");
+const snx = require("synthetix");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function(deployer, network) {
+  let synthetixAddress = snx.getTarget({
+    network: network.replace("-fork", ""),
+    contract: "Synthetix"
+  }).address;
+
+  deployer.deploy(SynthRebalancer, synthetixAddress);
 };
