459f8f5f46274134a33e5d98bf7afefb539fdf9b
opz <will.shahda@gmail.com>
2020-02-03 02:25:27 -0500

    Configure Truffle for Ropsten testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,11 @@
+require("dotenv").config();
+
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const dev_mnemonic = process.env.ETH_DEV_MNEMONIC;
+const infuraProjectId = process.env.INFURA_PROJECT_ID;
+const infuraUrl = network => `https://${network}.infura.io/v3/${infuraProjectId}`;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +14,10 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(dev_mnemonic, infuraUrl("ropsten")),
+      network_id: 3
     }
   },
   compilers: {
