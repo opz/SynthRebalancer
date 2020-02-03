444a80ba23ed0e63f0bddc7864207a9d187e7c65
opz <will.shahda@gmail.com>
2020-02-03 01:20:42 -0500

    Create interface for Synthetix contract
    Allows other contracts to interact with a deployed Synthetix contract.

---
+++ contracts/SynthetixInterface.sol
@@ -0,0 +1,35 @@
+pragma solidity >=0.5.15 <0.7.0;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SynthInterface is IERC20 {
+}
+
+contract SynthetixInterface {
+    SynthInterface[] public availableSynths;
+    mapping(bytes32 => SynthInterface) public synths;
+    mapping(address => bytes32) public synthsByAddress;
+
+    function effectiveValue(
+        bytes32 sourceCurrencyKey,
+        uint sourceAmount,
+        bytes32 destinationCurrencyKey
+    )
+        public
+        view
+        returns (uint)
+    {}
+
+    function totalIssuedSynths(bytes32 currencyKey)
+        public
+        view
+        returns (uint)
+    {}
+
+    function availableCurrencyKeys() public view returns (bytes32[] memory) {}
+
+    function exchange(bytes32 sourceCurrencyKey, uint sourceAmount, bytes32 destinationCurrencyKey)
+        external
+        returns (bool)
+    {}
+}
