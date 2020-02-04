0e376aa68951234e26d849c824366927fa527a4a
opz <will.shahda@gmail.com>
2020-02-04 18:34:57 -0500

    Update migration to handle networks where Synthetix has no deployment
    Used for development and test networks.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,18 @@
 const SynthRebalancer = artifacts.require("SynthRebalancer");
 const snx = require("synthetix");
+const { constants } = require("@openzeppelin/test-helpers");
 
 module.exports = function(deployer, network) {
-  let synthetixAddress = snx.getTarget({
-    network: network.replace("-fork", ""),
-    contract: "Synthetix"
-  }).address;
+  let synthetixAddress;
+
+  try {
+    synthetixAddress = snx.getTarget({
+      network: network.replace("-fork", ""),
+      contract: "Synthetix"
+    }).address;
+  } catch {
+    synthetixAddress = constants.ZERO_ADDRESS;
+  }
 
   deployer.deploy(SynthRebalancer, synthetixAddress);
 };
