2625079ae582ca2f39dc4ec57595af72dc722929
opz <will.shahda@gmail.com>
2020-02-04 18:39:32 -0500

    Disable mocha colors
    Needed to read output on some consoles.

--- truffle-config.js
+++ truffle-config.js
@@ -11,6 +11,9 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  mocha: {
+    useColors: false
+  },
   networks: {
     develop: {
       port: 8545
